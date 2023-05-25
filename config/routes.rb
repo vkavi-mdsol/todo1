7c378de3f8690deb2164b393d3fced0b2efd1549
vaishnavi kavi <vkavi@mdsol.com>
2023-05-25 17:59:24 +0530

    added ability to add todo list, add todo items to lists, and delete todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :todo_lists do
+     resources :todo_items
+    end
+  root "todo_lists#index"
 end
